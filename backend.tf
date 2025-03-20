435e902320259a8fc2244c8afd6928225cd20167
Ajay Kumar Mannam <ajaymannam97@gmail.com>
2025-03-19 22:49:40 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "terraformstorage098"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    resource_group_name  = "secret-resource"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "terraformstorage099"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "prodtfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
